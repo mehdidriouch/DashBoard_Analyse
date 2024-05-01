0a1d01b9f1caf8a13186ea98b11cb9d21e4ade20
mehdidriouch <gestion2011@gmail.com>
2024-05-01 02:14:22 +0200

    Update 01-05-2024
    Add template file

--- Dash/admin.py
+++ Dash/admin.py
@@ -1,5 +1,1 @@
 from django.contrib import admin
-from Dash.models import Exemple
-
-# Register your models here.
-admin.site.register(Exemple)
--- Dash/models.py
+++ Dash/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-# Create your models here.
-class Exemple(models.Model):
-    name = models.CharField(max_length=100, verbose_name="name")
-    age = models.IntegerField(max_length=10)
-    def __str__(self):
-        return str(self.name)
